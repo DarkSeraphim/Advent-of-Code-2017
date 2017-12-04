5fbd5bcb87686dfa64056ce168941156456de732
Mark Hendriks <datfireblast709@gmail.com>
2017-12-04 19:05:35 +0000

    And part 2

---
+++ 04/p2.groovy
@@ -0,0 +1,18 @@
+InputStream.metaClass.untilEnd = { closure ->
+    delegate.eachLine("UTF-8") {
+        if ("end-of-input".equals(it)) {
+            return;
+        }
+        closure(it)
+    }
+}
+
+counter = 0;
+System.in.untilEnd {
+  tokens = it.tokenize(' ').collect {
+    it.collect{it}.sort().join('')
+  }
+  if (tokens.toSet().size() == tokens.size()) counter++
+}
+
+println(counter)
