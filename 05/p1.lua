537b06f953873fa0fe2c67d189d8d698c853210e
Mark Hendriks <datfireblast709@gmail.com>
2017-12-05 15:17:40 +0000

    First part of lua

---
+++ 05/p1.lua
@@ -0,0 +1,16 @@
+instructions = {}
+line = io.read()
+while line ~= "end-of-input" do
+  instructions[#instructions + 1] = tonumber(line)
+  line = io.read()
+end
+
+IP = 1
+counter = 0
+while #instructions >= IP and IP >= 1 do
+  num = instructions[IP]
+  instructions[IP] = num + 1
+  IP = IP + num
+  counter = counter + 1
+end
+print(string.format("Took %d steps!", counter))
