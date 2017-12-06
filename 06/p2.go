c19125cf82e396755e76ccc88b73626b15e23895
Mark Hendriks <datfireblast709@gmail.com>
2017-12-06 19:22:13 +0000

    And part 2

---
+++ 06/p2.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+  var state [16]int
+  var seen = make(map[string]int)
+  parseArray(state[:])
+  var mutations = 0
+  fmt.Println(toString(state[:]))
+  fmt.Println()
+  for add(seen, state[:], mutations) {
+    var index, value = max(state[:])
+    state[index] = 0
+    var div = value / 16
+    var mod = value % 16
+    for i, v := range state {
+      var x = (i - index - 1 + 16) % 16
+      v += div
+      if x < mod {
+        v++
+      }
+      state[i] = v
+    }
+    mutations++
+  }
+  var val, _ = seen[toString(state[:])]
+  fmt.Printf("Found a cycle after %d mutations. Cycle size was %d.\n", mutations, mutations - val)
+}
+
+func parseArray(state []int) {
+  for i, _ := range state {
+    _, err := fmt.Scanf("%d", &state[i])
+    if err != nil {
+      fmt.Println("Err!")
+    }
+  }
+}
+
+func add(set map[string]int, key []int, when int) bool {
+  var sk = toString(key)
+  if _, ok := set[sk]; ok {
+    return false
+  }
+  set[sk] = when
+  return true
+}
+
+func toString(array []int) string {
+  var str = ""
+  for i, v := range array {
+    if i > 0 {
+      str += ","
+    }
+    str += fmt.Sprint(v)
+  }
+  return str
+}
+
+func max(array []int) (int, int) {
+  var value = -1
+  var index = -1
+  for i, v := range array {
+    if value < v {
+      value = v
+      index = i
+    }
+  }
+  return index, value
+}
